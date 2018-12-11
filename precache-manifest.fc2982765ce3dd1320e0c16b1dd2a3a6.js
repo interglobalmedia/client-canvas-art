8fc8e1e088a16c054afd65c8764a76b1a1b15fa2
interglobalmedia <interglobalmedia@gmail.com>
2018-12-10 23:07:27 -0500

    Updates

---
+++ precache-manifest.fc2982765ce3dd1320e0c16b1dd2a3a6.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = [
+  {
+    "url": "styles/main.5055c84964dd2c7dc421.css"
+  },
+  {
+    "revision": "046c433cdda55afe439a",
+    "url": "scripts/main.3c0bb68a125601db2f9a.chunk.js"
+  },
+  {
+    "revision": "52740080b7c02d5a24c7701f6984b26c",
+    "url": "index.html"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "4a88ac6a4f7c5ee1ec302ad29f12f73a",
+    "url": "images/bark.jpg"
+  }
+];
